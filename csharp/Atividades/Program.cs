754dcd0018a5427fdda4a201e06ee70f1428aa44
Marlos Ribeiro <marlos.ribeiro@ufpe.br>
2024-04-22 16:48:12 -0300

    Curso de C# - Modulos 1 e 2

---
+++ csharp/Atividades/Program.cs
@@ -0,0 +1,172 @@
+﻿using System.Reflection.Emit;
+
+void PrintSplashScreen()
+{
+    Console.WriteLine(@"
+█▀▄ █▀▀ █▀ ▄▀█ █▀▀ █ █▀█ █▀ ▀   █░█ █▀█ █▀█ ▄▀█   █▀▄ ▄▀█   █▀█ █▀█ ▄▀█ ▀█▀ █ █▀▀ ▄▀█
+█▄▀ ██▄ ▄█ █▀█ █▀░ █ █▄█ ▄█ ▄   █▀█ █▄█ █▀▄ █▀█   █▄▀ █▀█   █▀▀ █▀▄ █▀█ ░█░ █ █▄▄ █▀█
+    ");
+}
+
+void PrintListaDesafios(List<string> desafios)
+{
+    Console.WriteLine("Desafios disponíveis:");
+    for (int i = 0; i < desafios.Count; i++)
+    {
+        Console.WriteLine($"{i + 1} - {desafios[i]}");
+    }
+    Console.WriteLine("0 - Sair");
+}
+
+int LerOpcao()
+{
+    Console.Write("Digite a opção desejada: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintTitulo(string titulo)
+{
+    Console.WriteLine($"\n###\n{titulo}\n###");
+}
+
+void ChamarDesafio(int opc)
+{
+    switch (opc)
+    {
+        case 1:
+            Atividade_M01_ATV06();
+            break;
+        case 2:
+            Atividade_M02_ATV08();
+            break;
+        case 3:
+            Desafio_M02_ATV09();
+            break;
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+void Desafio_M02_ATV09()
+{
+    /*
+Neste jogo, o programa gera um número aleatório entre 1 e 100 e o usuário deve adivinhar qual é esse número. O programa utiliza o if-else e switch case para verificar se o número digitado pelo usuário é maior ou menor do que o número gerado pelo programa.
+
+O programa também utiliza o conceito de laço de repetição do-while para permitir que o usuário faça várias tentativas até acertar o número. Quando o usuário acertar o número, o jogo acaba e o programa exibe uma mensagem indicando que o jogo acabou.
+    */
+
+    PrintTitulo("Faça como eu fiz: primeiro jogo com C#");
+    Random random = new Random();
+    int numeroAleatorio = random.Next(1, 101);
+    int tentativas = 10;
+    int numeroUsuario = 0;
+    Console.WriteLine(@"
+    Bem-vindo ao jogo de adivinhação!
+    Tente adivinhar o número gerado pelo programa.
+    Você terá 10 tentativas para acertar o número.
+    ");
+
+    do
+    {   
+        if (tentativas == 0)
+        {
+            Console.WriteLine("Você perdeu! Suas tentativas acabaram.");
+            Console.WriteLine($"O número correto era {numeroAleatorio}");
+            return;
+        }
+        Console.WriteLine($"Você tem {tentativas} tentativas restantes.");
+        Console.Write("Digite um número entre 1 e 100: ");
+        numeroUsuario = int.Parse(Console.ReadLine()!);
+        tentativas--;
+        if (numeroUsuario < numeroAleatorio)
+        {
+            Console.WriteLine("O número é maior");
+        } else if (numeroUsuario > numeroAleatorio)
+        {
+            Console.WriteLine("O número é menor");
+        }
+    } while (numeroUsuario != numeroAleatorio);
+
+    if (numeroUsuario == numeroAleatorio)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número!");
+    }
+}
+
+void Atividade_M02_ATV08()
+{
+    /*
+1. Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".
+2. Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+3. Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+4. Criar um programa que, dado a entrada de dados de um número n inteiro, a partir do teclado, exibir a n-ésima posição de uma lista.    
+    */
+
+    PrintTitulo("Módulo 02 - Atividade 08");
+    int notaMedia = 6;
+    if (notaMedia >= 5)
+    {
+        Console.WriteLine("Nota suficiente para aprovação");
+    } else {
+        Console.WriteLine("Nota insuficiente para aprovação");
+    }
+
+    List<string> linguagens = new List<string> { "C#", "Java", "JavaScript" };
+    Console.WriteLine(linguagens[0]);
+    linguagens.ForEach(l => Console.WriteLine(l));
+    Console.Write("Digite um posição da lista exibida: ");
+    int opc = int.Parse(Console.ReadLine()!);
+    if (opc >= linguagens.Count || opc < 0)
+    {
+        Console.WriteLine("Posição inválida");
+        return;
+    } else {
+        Console.WriteLine(linguagens[opc]);
+    }
+
+}
+
+void Atividade_M01_ATV06()
+{
+    /*
+1. Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+2. Mostrar o conteúdo da variável curso no console.
+3. Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+4. Mostrar na tela o nome dos instrutores do curso.    
+    */
+    PrintTitulo("Módulo 01 - Atividade 06");
+    string curso = "C# Fundamentos";
+    Console.WriteLine($"Curso: {curso}");
+    string nome = "Fuloano";
+    string sobrenome = "da Silva";
+    Console.WriteLine($"Nome: {nome} {sobrenome}");
+    Console.WriteLine("Instrutores: Guilherme Lima e Daniel Portugal");
+}
+
+// Main program
+// -----------------
+List<string> desafios = new List<string> { 
+    "M01-AT06 :: Desafio: hora da prática", 
+    "M02-AT08 :: Desafio: hora da prática",
+    "M02-AT09 :: Faça como eu fiz: primeiro jogo com C#" };
+// -----------------
+
+while (true)
+{
+    Console.Clear();
+    PrintSplashScreen();
+    PrintListaDesafios(desafios);
+    int opc = LerOpcao();
+    ChamarDesafio(opc);
+    if (opc == 0)
+    {
+        break;
+    } else {
+        Console.WriteLine("Pressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+}
---
+++ csharp/ScreenSound/Program.cs
@@ -0,0 +1,73 @@
+﻿// PRATICAS em video
+
+void PrintMensagemTitulo(string mensagem)
+{
+    Console.WriteLine($"###\n{mensagem}\n###");
+}
+
+void PrintBoasVindas()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░    
+    ");
+    
+    PrintMensagemTitulo("Bem vindo!");
+}
+
+void PrintMenu()
+{
+    Console.WriteLine(@"
+    1 - Registrar uma banda
+    2 - Mostrar todas as bandas
+    3 - Avaliar uma banda
+    4 - Mostrar média de avaliações de uma banda
+    0 - Sair
+
+    ");
+}
+
+int LerOpcao()
+{
+    Console.Write("Digite a opção desejada:");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void AvaliarOpcaoEscolhida(int opc)
+{
+    switch (opc)
+    {
+        case 1:
+            PrintMensagemTitulo("Registrar uma banda");
+            break;
+        case 2:
+            PrintMensagemTitulo("Mostrar todas as bandas");
+            break;
+        case 3:
+            PrintMensagemTitulo("Avaliar uma banda");
+            break;
+        case 4:
+            PrintMensagemTitulo("Mostrar média de avaliações de uma banda");
+            break;
+        case 0:
+            PrintMensagemTitulo("Saindo...");
+            break;
+        default:
+            PrintMensagemTitulo("Opção inválida");
+            break;
+    }
+
+}
+
+// -----------------------------------------------------------------------------
+// Inicio do programa
+PrintBoasVindas();
+PrintMenu();
+int opcEscolhida = LerOpcao();
+AvaliarOpcaoEscolhida(opcEscolhida);
+
